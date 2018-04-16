8d59d9572ee0fa5129d6d8440df09f3e42918806
Marko Galevski <galevski.marko@gmail.com>
2018-04-16 13:11:51 +0200

    made it work with 2.0

---
+++ cmake-build-debug/CMakeFiles/c_uart_interface_example.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marko/VDX/c_uart_interface_example/autopilot_interface.cpp" "/home/marko/VDX/c_uart_interface_example/cmake-build-debug/CMakeFiles/c_uart_interface_example.dir/autopilot_interface.cpp.o"
+  "/home/marko/VDX/c_uart_interface_example/mavlink_control.cpp" "/home/marko/VDX/c_uart_interface_example/cmake-build-debug/CMakeFiles/c_uart_interface_example.dir/mavlink_control.cpp.o"
+  "/home/marko/VDX/c_uart_interface_example/serial_port.cpp" "/home/marko/VDX/c_uart_interface_example/cmake-build-debug/CMakeFiles/c_uart_interface_example.dir/serial_port.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../mavlink"
+  "../mavlink/include"
+  "../mavlink/include/mavlink"
+  "../mavlink/include/mavlink/v2.0"
+  "../mavlink/include/mavlink/v2.0/ardupilotmega"
+  "../mavlink/include/mavlink/v2.0/ASLUAV"
+  "../mavlink/include/mavlink/v2.0/autoquad"
+  "../mavlink/include/mavlink/v2.0/common"
+  "../mavlink/include/mavlink/v2.0/icarous"
+  "../mavlink/include/mavlink/v2.0/matrixpilot"
+  "../mavlink/include/mavlink/v2.0/minimal"
+  "../mavlink/include/mavlink/v2.0/slugs"
+  "../mavlink/include/mavlink/v2.0/standard"
+  "../mavlink/include/mavlink/v2.0/test"
+  "../mavlink/include/mavlink/v2.0/uAvionix"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/c_uart_interface_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_uart_interface_example.dir/autopilot_interface.cpp.o"
+  "CMakeFiles/c_uart_interface_example.dir/mavlink_control.cpp.o"
+  "CMakeFiles/c_uart_interface_example.dir/serial_port.cpp.o"
+  "c_uart_interface_example.pdb"
+  "c_uart_interface_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_uart_interface_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
